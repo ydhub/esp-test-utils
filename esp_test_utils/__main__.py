025913e69459b3a9e77e26946573e6310cc75996
Chen Yu Dong <chenyudong@espressif.com>
2024-03-20 16:25:40 +0800

    feat(init): basic template python package

---
+++ esp_test_utils/__main__.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def main() -> None:
+    print(f'Current python version: {sys.version}')
+    assert sys.version_info.major == 3, 'Only support python3'
+    if sys.version_info.minor < 8:
+        import pkg_resources
+
+        package_ver = pkg_resources.get_distribution('esp_test_utils').version
+    else:
+        from importlib.metadata import version
+
+        package_ver = version('esp_test_utils')
+    print(f'Installed esp_test_utils version: {package_ver}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_core.py
@@ -0,0 +1,5 @@
+from esp_test_utils.__main__ import main
+
+
+def test_main() -> None:
+    main()
