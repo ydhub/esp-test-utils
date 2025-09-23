a895f160d2d5017739cac3ec68637e161976c406
You Wei <youwei@espressif.com>
2025-09-23 11:20:24 +0800

    chore(downbin): refine the usage message and fix issue

--- esptest/scripts/downbin.py
+++ esptest/scripts/downbin.py
@@ -9,15 +9,18 @@ from esptest.tools.download_bin import bin_path_to_dir, download_bin_to_ports
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description='Download bin')
+    usage_string = '%(prog)s [bin_path] [options]'
+    parser = argparse.ArgumentParser(description='Download bin', usage=usage_string)
     parser.add_argument('bin_path', type=str, nargs='?', help='esp bin path, default ./build')
     parser.add_argument('-p', '--ports', type=str, nargs='*', help='download port list')
     parser.add_argument(
         '--range', type=str, help='port list from range (linux), eg: "0-10" equals to "-p ttyUSB0 ttyUSB1 ... ttyUSB10"'
     )
-    parser.add_argument('--all', action='store_true', help='download to all serial ports.')
-    parser.add_argument('--erase-nvs', type=bool, default=True, help='use --erase-nvs=n to skip erase nvs')
-    parser.add_argument('--max-workers', type=int, default=True, help='max download threads')
+    parser.add_argument(
+        '--all', action='store_true', help='download to all serial ports, ignored if "-p/--ports" is specified.'
+    )
+    parser.add_argument('--no-erase-nvs', dest='erase_nvs', action='store_false', help='skip erase nvs')
+    parser.add_argument('--max-workers', type=int, default=0, help='max download threads')
     args = parser.parse_args()
 
     bin_path = args.bin_path or './build'
@@ -42,9 +45,13 @@ def main() -> None:
         ports = [os.getenv('ESPPORT') or '/dev/ttyUSB0']
     assert isinstance(ports, list)
 
-    logging.critical(f'Download {bin_path} to {ports}')
-    download_bin_to_ports(bin_path, ports, args.erase_nvs, args.max_workers)
+    try:
+        download_bin_to_ports(bin_path, ports, args.erase_nvs, args.max_workers)
+    except RuntimeError as e:
+        logging.error(str(e))
+        sys.exit(1)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
     main()
